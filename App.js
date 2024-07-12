0235dfcb165c513506247bc2cf357513eadcc624
Raguru Hemalatha <raguruhemalatha277@gmail.com>
2024-07-12 14:02:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Portfolio from './portfoliowebsite';
+
+function App() {
+  return (
+    <div className="App">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
